3abeaf3f2b2c9783842a8d62d965fde8c41b0305
student <ishaanpatel@billericak12.com>
2021-09-22 11:22:45 -0400

    w

---
+++ src/com/company/NewRandom.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class NewRandom {
+
+
+    public static void main(String[] args) {
+
+        double r = Math.random();
+        System.out.println(r);
+        r = Math.random() * 10;
+        System.out.println(r);
+        int p = (int)(Math.random()*11);
+        System.out.println(p);
+        p = (int)(Math.random()*11);
+        System.out.println(p);
+        p = (int)(Math.random()*11);
+        System.out.println(p);
+        p = (int)(Math.random()*11);
+        System.out.println(p);
+        p = (int)(Math.random()*11)+10;
+        System.out.println(p);
+        p = (int)(Math.random()*11)+10;
+        System.out.println(p);
+        p = (int)(Math.random()*11)+10;
+        System.out.println(p);
+        p = (int)(Math.random()*11)+10;
+        System.out.println(p);
+        p = (int)(Math.random()*11)+21; // [21. 30]
+        System.out.println(p);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
